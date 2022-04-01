87fc0862310fd468a7c2b2e8104e74c0084dc92c
FangfangHao <fangfanghao@seismic.com>
2022-04-01 17:34:18 +0800

    add introspection to server

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
 const {ApolloServer, gql} = require('apollo-server');
 const resolvers = require('./resolvers');
 const typeDefs = gql(require('./typeDefs'));
-
-// const books = [
-//     {
-//       title: 'The Awakening',
-//       author: 'Kate Chopin',
-//     },
-//     {
-//       title: 'City of Glass',
-//       author: 'Paul Auster',
-//     },
-//   ];
-  
-//   // Schema definition
-//   const typeDefs = gql`
-//     type Book {
-//       title: String
-//       author: String
-//     }
-  
-//     type Query {
-//       books: [Book]
-//     }
-//   `;
-  
-//   // Resolver map
-//   const resolvers = {
-//     Query: {
-//       books() {
-//         return books;
-//       }
-//     },
-//   };
   
 async function startApolloServer(typeDefs, resolvers, introspection = false) {
     const server = new ApolloServer({
@@ -45,4 +13,4 @@ async function startApolloServer(typeDefs, resolvers, introspection = false) {
     console.log(`🚀  Server ready at ${url}`);
 }
 
-const server = startApolloServer(typeDefs, resolvers);
+const server = startApolloServer(typeDefs, resolvers, true);
